ef55a7cbb977be0e59e35f250ea91e8b60cd9c6c
purplevarun <purplevarun@gmail.com>
2021-01-24 16:36:46 +0530

    .

---
+++ dp/3-canSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+class canSum{
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int a[]=new int[args.length-1];
+		for(int i=0,j=1;j<args.length;j++)
+			a[i++] = Integer.parseInt(args[j]);
+		int size = args.length-1;
+		
+		System.out.println(cansum(n,a,new HashMap<Integer,Boolean>()));
+
+	}
+	static boolean cansum(int n,int a[],HashMap<Integer,Boolean> dp){
+		if (dp.containsKey(n)) return dp.get(n);	
+		if (n==0) return true;
+		if (n<0) return false;
+		for(int i:a){
+			if(cansum(n-i,a,dp)==true) {
+				dp.put(n,true);
+				return true;
+			}
+		}
+		dp.put(n,false);
+		return false;
+	}
+}
