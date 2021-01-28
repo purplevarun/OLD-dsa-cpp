52e5c222263b47073373dcdc3af737204b77c2d4
purplevarun <purplevarun@gmail.com>
2021-01-28 16:58:57 +0530

    .

--- dp/6-canWordCount.py
+++ dp/6-canWordCount.py
@@ -11,5 +11,8 @@ def canWord(target,wordBank,dp):
 	dp[target] = count
 	return count
 if __name__ == '__main__':
-	print(canWord("abcdef",["ab","abc","cd","def","abcd","ef"],dp={}))
-	print(canWord("e"*100+"f",["e","eeee","eeeeeeee","eeeeee"],dp={}))
+	# print(canWord(input(),[x for x in input().split()],dp={}),)
+	print(canWord("abcdefgh",["abc","abcd","abcdefg","def","hh","gh","ef","h"],dp={}))
+	print(canWord("abcdefgh",["abc","abcd","abcdefg","def","hh","gh","ef","h","g"],dp={}))
+	# print(canWord("abcdef",["ab","abc","cd","def","abcd","ef"],dp={}))
+	# print(canWord("e"*100+"f",["e","eeee","eeeeeeee","eeeeee"],dp={}))
---
+++ dp/7-all-ways-to-make-word.py
@@ -0,0 +1,30 @@
+def allWays(target,wordBank,dp={}):
+	if target == "": return [[]]
+	result = []
+	for i in wordBank:
+		if target.startswith(i):
+			suffix = target[len(i):]
+			ways = allWays(suffix,wordBank,dp)
+			for x in ways : x.insert(0,i)
+			result=result+ways
+	return result		
+if __name__ == '__main__':
+	
+	s1 = "abcd"
+	s2 = "abcdefgh"
+	s3 = "e"*10+"f"
+	arr1 = ["cd","a","ab","b","bcd"]
+	arr2 = ["abc","abcd","abcdefg","def","hh","gh","ef","h","g"]
+	arr3 = ["ef","eaaee","ee","eeeeeee","fff","f"]
+	
+	# ans1 = allWays(s1,arr1)
+	# for x in ans1:
+	# 	print(x)
+	
+	ans2 = allWays(s2,arr2)
+	for x in ans2:
+		print(x)
+
+	# ans3 = allWays(s3,arr3)
+	# for x in ans3:
+	# 	print(x)
