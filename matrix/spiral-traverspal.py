96f52398a7ec99169a97f0d4c64a76b304b2f5b4
purplevarun <purplevarun@gmail.com>
2021-02-12 12:38:53 +0530

    .

---
+++ matrix/spiral-traverspal.py
@@ -0,0 +1,7 @@
+def spiralTraversal(matrix) :
+    rows = len(matrix)
+    cols = len(matrix[0])
+    i = 0 # starting row index
+    j = 0 # starting column index
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+print (spiralTraversal(matrix))
