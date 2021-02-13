2786d28e859c52b7cfa17dc2d7aceffb5f9e7ba8
purplevarun <purplevarun@gmail.com>
2021-02-13 13:17:48 +0530

    .

--- tree/1-binary-tree.py
+++ tree/1-binary-tree.py
@@ -1,21 +1,13 @@
 class node :
-    value = None
-    left,right = None,None
     def __init__(this,value):
         this.value = value
-        left,right = None,None
+        this.left,this.right = None,None
 def preorder(root):
     if not root : return
     print (root.value, end=" ")
     preorder (root.left)
     preorder (root.right)
-def insert(root,x):
-    if not root : root = node(x)
-    else :
-        
-root = node(1)
-root.left = node(2)
-root.right = node(3)
-root.left.left = node(4)
-root.left.right = node(5)
+
+root = node(None)
+root = node(5)
 preorder(root)
