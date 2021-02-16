4a60a1cc5740bd93b4410e06c23dbcfda5df2ea4
purplevarun <purplevarun@gmail.com>
2021-02-16 09:57:12 +0530

    .

--- tree/1-binary-tree.cpp
+++ tree/1-binary-tree.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
-#define nullptr null
+#define null nullptr
 using namespace std;
 class node {
+    public:
     int value;
     node *left,*right;
     public: node(int value){
-        this.value = value;
+        this->value = value;
+        this->left = this->right = null;
     }
 };
 int main () {
-    
+    node* root = new node(5);
+    cout << root << endl;
+    cout << root->value << endl;
+    cout << root->left << endl;
 }
