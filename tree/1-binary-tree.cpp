8325f0e34e8f06d7d11535046ad2d9dfab1a0c05
purplevarun <purplevarun@gmail.com>
2021-02-16 09:52:12 +0530

    .

---
+++ tree/1-binary-tree.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define nullptr null
+using namespace std;
+class node {
+    int value;
+    node *left,*right;
+    public: node(int value){
+        this.value = value;
+    }
+};
+int main () {
+    
+}
