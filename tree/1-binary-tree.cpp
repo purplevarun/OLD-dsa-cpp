9bde36c1793d31aad7e5b0be01b1e12d5ec0d66b
purplevarun <purplevarun@gmail.com>
2021-02-16 10:04:21 +0530

    .

--- tree/1-binary-tree.cpp
+++ tree/1-binary-tree.cpp
@@ -10,7 +10,17 @@ class node {
         this->left = this->right = null;
     }
 };
+void preorder (node* root){
+    if (root == null) return;
+    cout << root -> value << endl;
+    preorder (root -> left);
+    preorder (root -> right);
+}
 int main () {
     node* root = null;
-    
+    root = new node (3);
+    root -> left = new node (5);
+    root -> right = new node (2);
+    root -> right -> left = new node (10);
+    preorder (root);
 }
