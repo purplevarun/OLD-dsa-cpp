053d7286ccad6904042b6ec973b61a85c2b772c4
purplevarun <purplevarun@gmail.com>
2021-02-17 22:24:22 +0530

    .

--- stack/1-next-largest-element.cpp
+++ stack/1-next-largest-element.cpp
@@ -6,6 +6,10 @@ using namespace std;
 signed main () {
     // Nearest greatest to the right
     vi A {1,3,2,4}; // TC 1 Answer = {3,4,4,-1}
-    vi A {1,3,0,0,1,2,4} // TC 2 Answer = {3,4,1,1,2,4,-1}
+    // vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {3,4,1,1,2,4,-1}
+    int N = A.size();
+    vi Result;
+    //-------------------------
+    // Brute Force
     
 }
