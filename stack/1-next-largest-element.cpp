e17604135a2f6724854884211bac491d60ec8e14
purplevarun <purplevarun@gmail.com>
2021-02-17 22:22:00 +0530

    .

--- stack/1-next-largest-element.cpp
+++ stack/1-next-largest-element.cpp
@@ -4,5 +4,8 @@ using namespace std;
 #define null nullptr
 #define vi vector <int>
 signed main () {
+    // Nearest greatest to the right
+    vi A {1,3,2,4}; // TC 1 Answer = {3,4,4,-1}
+    vi A {1,3,0,0,1,2,4} // TC 2 Answer = {3,4,1,1,2,4,-1}
     
 }
