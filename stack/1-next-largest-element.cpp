f3f11785c175c095ec9d2dbf145baafe95bfe4ec
purplevarun <purplevarun@gmail.com>
2021-02-17 22:29:38 +0530

    .

--- stack/1-next-largest-element.cpp
+++ stack/1-next-largest-element.cpp
@@ -3,6 +3,11 @@ using namespace std;
 #define int long long int
 #define null nullptr
 #define vi vector <int>
+void print (vi X){
+    for (int i=0;i<X.size()-1;i++)
+        cout << X[i] << ", ";
+    cout << X[X.size()-1] << endl;
+}
 signed main () {
     // Nearest greatest to the right
     vi A {1,3,2,4}; // TC 1 Answer = {3,4,4,-1}
@@ -11,5 +16,18 @@ signed main () {
     vi Result;
     //-------------------------
     // Brute Force
-    
+    for (int i=0;i<N;i++) {
+        int key = A[i];
+        bool check = false;
+        for (int j=i+1;j<N;j++) {
+            int X = A[j];
+            if (X > key) {
+                Result.push_back(X);
+                check = true;
+                break;
+            }
+        }
+        if (!check) Result.push_back(-1);
+    }
+    print (Result);
 }
