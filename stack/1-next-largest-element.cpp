a67daa1f3dcfb029d6fc4e3651a520481dee8ed5
purplevarun <purplevarun@gmail.com>
2021-02-17 22:17:32 +0530

    .

---
+++ stack/1-next-largest-element.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define null nullptr
+#define vi vector <int>
+signed main () {
+    cout << sizeof (int) << "," << sizeof (signed) <<endl;
+}
