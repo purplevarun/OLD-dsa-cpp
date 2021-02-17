c6256ab8d7547bb909f0535836692e23b61237d8
purplevarun <purplevarun@gmail.com>
2021-02-17 22:05:18 +0530

    .

--- stack/1-next-largest-element.py
+++ stack/1-next-largest-element.py
@@ -1,11 +1,11 @@
 # nearest greater to the right
 A = [1,3,2,4] # result = [3,4,4,-1] # TC 1
 # A = [1,3,0,0,1,2,4] # result = [3,4,1,1,2,4,-1] # TC 2
-# N = len (A)
+N = len (A)
 # ----------------------------------------------
 Result = []
 stack = []
-
+for i in range (N-1,-1,-1): print (A[i])
 # ----------------------------------------------
 # BRUTE FORCE
 # Result = []
