d52d56a0e9799d3e181b9706b4036c71843685cd
purplevarun <purplevarun@gmail.com>
2021-02-17 21:00:17 +0530

    .

--- stack/1-next-largest-element.py
+++ stack/1-next-largest-element.py
@@ -1,2 +1,5 @@
 # nearest greater to the right
-A = [1,3,2,4] # result = [3,4,4,-1]
+# A = [1,3,2,4] # result = [3,4,4,-1] # TC 1
+A = [1,3,0,0,1,2,4] # result = [3,4,1,1,2,4,-1] # TC 2
+# brute force
+
