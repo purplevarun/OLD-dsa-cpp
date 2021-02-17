6ade88ffdda61b1ea0eb9da40fdc456b5258849f
purplevarun <purplevarun@gmail.com>
2021-02-17 09:07:51 +0530

    .

--- tree/1-binary-tree.py
+++ tree/1-binary-tree.py
@@ -11,13 +11,24 @@ def inorder (root):
     inorder (root.right)
 def insert (root, x):
     if not root: 
-        print ("root is null")
         root = node (x)
-        return
+        return root
+    else:
+        q = [] # empty queue
+        q.append(root)
+        while (q != []):
+            temp = q[0]
+            del q[0] # remove first element from queue
+            if not temp.left:
+                temp.left = node (x)
+                break
+            else: q.append (temp.left)
 
+            if not temp.right:
+                temp.right = node (x)
+                break
+            else: q.append (temp.right)
+    return root
 root = None
-root = node (1)
-root.left = node (2)
-root.right = node (3)
-inorder (root)
-insert (root,10)
+for i in range (1,7+1): root = insert (root, i)
+inorder(root)
