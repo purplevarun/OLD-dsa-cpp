05d1664a8d14f0f0926b7a7937319719011f2cfb
purplevarun <purplevarun@gmail.com>
2021-02-17 08:54:37 +0530

    .

--- tree/1-binary-tree.py
+++ tree/1-binary-tree.py
@@ -1,2 +1,10 @@
 class node:
-    
+    value = 0
+    left = None
+    right = None
+    def __init__ (this,value=0):
+        this.value = value
+
+root = node ()
+print (root)
+print (root.value)
