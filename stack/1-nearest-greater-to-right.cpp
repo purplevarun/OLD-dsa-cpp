678b9ab7e008060a2a0188488904aa365a1910fc
purplevarun <purplevarun@gmail.com>
2021-02-19 00:47:41 +0530

    .

--- stack/1-nearest-greater-to-right.cpp
+++ stack/1-nearest-greater-to-right.cpp
@@ -17,8 +17,8 @@ void print (vi X){
     cout << X[X.size()-1] << endl;
 }
 signed main () {
-    // vi A {1,3,2,4}; // TC 1 Answer = {3,4,4,-1}
-    vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {3,4,1,1,2,4,-1}
+    vi A {1,3,2,4}; // TC 1 Answer = {3,4,4,-1}
+    // vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {3,4,1,1,2,4,-1}
     // vi A {3,2,1}; // TC 3 Answer = {-1,-1,-1}
     // vi A {17,18,5,4,6,1}; // TC 4 Answer = {18,-1,6,6,-1,-1}
     int N = A.size();
