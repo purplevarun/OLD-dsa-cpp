6dfb0687f733a96fa384ec530d08373081b9d650
purplevarun <purplevarun@gmail.com>
2021-02-19 00:16:11 +0530

    .

--- stack/2-nearest-greater-to-left.cpp
+++ stack/2-nearest-greater-to-left.cpp
@@ -16,23 +16,25 @@ void print (vi X){
 }
 signed main () {
     // vi A {1,3,2,4}; // TC 1 Answer = {-1,-1,3,-1}
-    vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {-1,-1,3,3,3,3,-1}
+    // vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {-1,-1,3,3,3,3,-1}
+    vi A {3,2,1}; // TC 3 Answer = {-1,3,2}
+    // vi A {17,18,5,4,6,1}; // TC 4 Answer = {-1,-1,18,5,18,6}
     int N = A.size();
     vi Result;
     // -------------------------------------
     // BRUTE FORCE
-    // for (int i=0;i<N;i++) {
-    //     int key = A [i];
-    //     bool check = false;
-    //     int pos;
-    //     for (int j=0;j<i;j++) {
-    //         if (A[j]>key) {
-    //             check = true;
-    //             pos = j;
-    //         }
-    //     }
-    //     if (check) Result.push_back (A[pos]);
-    //     else Result.push_back (-1); 
-    // }
-    // print (Result);
+    for (int i=0;i<N;i++) {
+        int key = A [i];
+        bool check = false;
+        int pos;
+        for (int j=0;j<i;j++) {
+            if (A[j]>key) {
+                check = true;
+                pos = j;
+            }
+        }
+        if (check) Result.push_back (A[pos]);
+        else Result.push_back (-1); 
+    }
+    print (Result);
 }
