4c058a9f136b782ce5789c94b1de851e5a0d7c64
purplevarun <purplevarun@gmail.com>
2021-02-19 00:09:08 +0530

    .

---
+++ stack/2-nearest-greater-to-left.cpp
@@ -0,0 +1,13 @@
+/*
+    Nearest Greater to Left
+    Stack Question 2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define null nullptr
+#define vi vector <int>
+#define all(x) x.begin(),x.end()
+signed main () {
+    vi A {1,3,2,4}; // TC 1 Answer = {-1,-1,3,-1}
+}
