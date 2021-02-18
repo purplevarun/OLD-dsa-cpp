a200c46b4850fad5ce35afb21f3bd95451ec0dce
purplevarun <purplevarun@gmail.com>
2021-02-19 00:28:10 +0530

    .

--- stack/2-nearest-greater-to-left.cpp
+++ stack/2-nearest-greater-to-left.cpp
@@ -17,31 +17,32 @@ void print (vi X){
 }
 signed main () {
     // vi A {1,3,2,4}; // TC 1 Answer = {-1,-1,3,-1}
-    // vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {-1,-1,3,3,3,3,-1}
+    vi A {1,3,0,0,1,2,4}; // TC 2 Answer = {-1,-1,3,3,3,3,-1}
     // vi A {3,2,1}; // TC 3 Answer = {-1,3,2}
-    vi A {17,18,5,4,6,1}; // TC 4 Answer = {-1,-1,18,5,18,6}
+    // vi A {17,18,5,4,6,1}; // TC 4 Answer = {-1,-1,18,5,18,6}
     int N = A.size();
     vi Result;
     // -------------------------------------
-    stack <int> Stack;
-    for (int i=0;i<n;i++) {
-        if (Stack.empty()) {
+    stack <int> s;
+    for (int i=0;i<N;i++) {
+        if (s.empty()) {
             Result.pb(-1);
         }
         else {
-            if (Stack.top() > A[i]){
-                Result.pb(Stack.top());
+            if (s.top() > A[i]){
+                Result.pb(s.top());
             }
             else {
-                while (Stack.empty()==false && Stack.top() <= A[i]){
-                    Stack.pop();
+                while (s.empty()==false && s.top() <= A[i]){
+                    s.pop();
                 }
-                
+                if (s.empty()) Result.pb(-1);
+                else Result.pb(s.top());
             }
         }
-        Stack.push(A[i]);
-        
+        s.push(A[i]);
     }
+    print (Result);
     // -------------------------------------
     // BRUTE FORCE
     // for (int i=0;i<N;i++) {
