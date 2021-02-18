4c18a1f828422e1353eff4b3588ccb851e60f235
purplevarun <purplevarun@gmail.com>
2021-02-19 00:09:32 +0530

    .

--- stack/2-nearest-greater-to-left.cpp
+++ stack/2-nearest-greater-to-left.cpp
@@ -8,6 +8,12 @@ using namespace std;
 #define null nullptr
 #define vi vector <int>
 #define all(x) x.begin(),x.end()
+void print (vi X){
+    if (X.size()==0) return;
+    for (int i=0;i<X.size()-1;i++)
+        cout << X[i] << ", ";
+    cout << X[X.size()-1] << endl;
+}
 signed main () {
     vi A {1,3,2,4}; // TC 1 Answer = {-1,-1,3,-1}
 }
