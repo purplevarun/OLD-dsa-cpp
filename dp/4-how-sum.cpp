2c4ac5eed77241333709d23c0ffbd58e38567ebb
purplevarun <purplevarun@gmail.com>
2021-02-19 22:12:10 +0530

    .

--- dp/4-how-sum.cpp
+++ dp/4-how-sum.cpp
@@ -5,34 +5,16 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
-void print (vi X) {
-    int n = X.size();
-    if (n == 1 && X[0] == -1) {cout << "Not Possible";return;}
-    for (int i=0;i<n-1;i++) cout << X[i] << ", ";
-    cout << X[n-1];
-}
 vi howSum (int N, vi A) {
-    // cout << "\nA = " << endl;
-    print (A);
-    if (N == 0) return {};
-    if (N < 0) return {-1};
-    for (int i:A) {
-        vi B = howSum (N-i,A);
-        if (B[0]!=-1){
-            B.pb(i);
-            return B;
-        }        
-    }
-    return {-1};
+    // vi temp;
+    // if (N == 0) return temp;
+    return NULL;
 }
 int32_t main () {
     int N = 7; vi A {2,3}; // TC 1 Answer = {3,2,2}
     // int N = 9; vi A {2,4}; // TC 2 Answer = Not Possible
     // int N = 300; vi A {7,14}; // TC 3 Answer = Not Possible
     // int N = 100; vi A {2,48,1,3}; // TC 4 Answer = {48,48,3,1}
-    sort (all(A));
-    reverse (all(A));
-    // vi result = howSum (N,A);
-    // print (result);
-    print (A);
+    sort (all(A)); reverse (all(A));
+    vi result = howSum (N,A);
 }
