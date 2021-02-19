7bd7b4ceeab7562afed6d23e00c46b91cc398ee3
purplevarun <purplevarun@gmail.com>
2021-02-19 22:30:11 +0530

    .

--- dp/4-how-sum.cpp
+++ dp/4-how-sum.cpp
@@ -9,24 +9,28 @@ bool isWrong (vi X){
     if (X.size()==1 && (X[0] == -1))return true;
     return false;
 }
+map <int,vi> dp;
 vi howSum (int N, vi A) {
     vi empty;
     vi wrong {-1};
+    if (dp.find(N)!=dp.end()) return dp[N];
     if (N == 0) return empty;
     if (N < 0) return wrong;
     for (int i:A) {
         vi B = howSum (N-i,A);
         if (!isWrong(B)){
             B.pb(i);
+            dp[N] = B;
             return B;
         }
     }
+    dp[N] = wrong;
     return wrong;
 }
 int32_t main () {
     // int N = 7; vi A {2,3}; // TC 1 Answer = {3,2,2}
-    int N = 9; vi A {2,4}; // TC 2 Answer = Not Possible
-    // int N = 300; vi A {7,14}; // TC 3 Answer = Not Possible
+    // int N = 9; vi A {2,4}; // TC 2 Answer = Not Possible
+    int N = 300; vi A {7,14}; // TC 3 Answer = Not Possible
     // int N = 100; vi A {2,48,1,3}; // TC 4 Answer = {48,48,3,1}
     sort (all(A)); reverse (all(A));
     vi result = howSum (N,A);
