4f57d8f43a53a31f1a92d87cdd724c502e0db2a7
purplevarun <purplevarun@gmail.com>
2021-02-19 21:24:39 +0530

    .

--- dp/3-can-sum.cpp
+++ dp/3-can-sum.cpp
@@ -5,18 +5,24 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
-bool canSum (int target, vi A) {
-    if (target == 0) return true;
-    if (target < 0) return false;
+map <int,bool> dp;
+bool canSum (int N, vi A) {
+    if (dp.find(N)!=dp.end()) return dp[N];
+    if (N == 0) return true;
+    if (N < 0) return false;
     for (int i:A) {
-        if (canSum (target - i, A)) return true;
+        if (canSum (N - i, A)) {
+            dp [N] = true;
+            return true;
+        }
     }
+    dp [N] = false;
     return false;
 }
 int32_t main () {
-    int target = 7; vi A {2,3}; // TC 1 Answer = Yes
-    // int target = 9; vi A {2,4}; // TC 2 Answer = No
-    // int target = 300; vi A {7,14}; // TC 3 Answer = No
-    if (canSum(target,A)) cout << "Yes";
+    // int N = 7; vi A {2,3}; // TC 1 Answer = Yes
+    // int N = 9; vi A {2,4}; // TC 2 Answer = No
+    int N = 300; vi A {7,14}; // TC 3 Answer = No
+    if (canSum(N,A)) cout << "Yes";
     else cout << "No";
 }
