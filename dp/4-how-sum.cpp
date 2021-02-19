f480ea631d4c539f2bc16beca7a46aec13061284
purplevarun <purplevarun@gmail.com>
2021-02-19 22:36:19 +0530

    .

--- dp/4-how-sum.cpp
+++ dp/4-how-sum.cpp
@@ -5,10 +5,6 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
-bool isWrong (vi X){
-    if (X.size()==1 && (X[0] == -1))return true;
-    return false;
-}
 vi empty;
 vi wrong {-1};
 map <int,vi> dp;
@@ -18,7 +14,7 @@ vi howSum (int N, vi A) {
     if (N < 0) return wrong;
     for (int i:A) {
         vi B = howSum (N-i,A);
-        if (!isWrong(B)){
+        if (B != wrong){
             B.pb(i);
             dp[N] = B;
             return B;
@@ -30,8 +26,9 @@ vi howSum (int N, vi A) {
 int32_t main () {
     // int N = 7; vi A {2,3}; // TC 1 Answer = {3,2,2}
     // int N = 9; vi A {2,4}; // TC 2 Answer = Not Possible
-    int N = 300; vi A {7,14}; // TC 3 Answer = Not Possible
-    // int N = 100; vi A {2,48,1,3}; // TC 4 Answer = {48,48,3,1}
+    // int N = 300; vi A {7,14}; // TC 3 Answer = Not Possible
+    int N = 100; vi A {2,48,1,3}; // TC 4 Answer = {48,48,3,1}
+    // int N = 50; vi A {1,3}; // TC 5
     sort (all(A)); reverse (all(A));
     vi result = howSum (N,A);
     reverse (all(result));
