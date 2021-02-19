9865b08d81d44ac8edaae70533536622f032b974
purplevarun <purplevarun@gmail.com>
2021-02-19 23:32:23 +0530

    .

--- dp/7-all-ways-to-make-word.py
+++ dp/7-all-ways-to-make-word.py
@@ -16,13 +16,13 @@ if __name__ == '__main__':
 	arr2 = ["abc","abcd","abcdefg","def","hh","gh","ef","h","g"]
 	arr3 = ["ef","eaaee","ee","eeeeeee","fff","f"]
 	
-	# ans1 = allWays(s1,arr1)
-	# for x in ans1:
-	# 	print(x)
-	
-	ans2 = allWays(s2,arr2)
-	for x in ans2:
+	ans1 = allWays(s1,arr1)
+	for x in ans1:
 		print(x)
+	
+	# ans2 = allWays(s2,arr2)
+	# for x in ans2:
+	# 	print(x)
 
 	# ans3 = allWays(s3,arr3)
 	# for x in ans3:
