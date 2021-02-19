5ebd558065aa78307beb9a825e1de9198b8ad231
purplevarun <purplevarun@gmail.com>
2021-02-19 21:03:55 +0530

    .

---
+++ dp/3-can-sum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define null nullptr
+#define vi vector <int>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+int32_t main (int32_t argc, char** argv) {
+    int target = atoi (argv[1]);
+    vi arr;
+    for (int i=2;i<argc;i++) arr.pb(atoi(argv[i]));
+}
