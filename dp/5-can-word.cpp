07432971d4c757850bf9b230b2e51f1504ebd5d6
purplevarun <purplevarun@gmail.com>
2021-02-19 23:00:36 +0530

    .

---
+++ dp/5-can-word.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define null nullptr
+#define vs vector <string>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+bool canWord (string S, vs A) {
+    if (S == "") return true;
+    for (string i : A) {
+        if (S.rfind(i,0)==0) { // if S starts with i
+            if (canWord(S.substr(i.length()),A)) return true;
+        }
+    }
+    return false;
+}
+int32_t main () {
+    // TC 1 - Yes
+    string S = "abcdef";
+    vs A {"ab","abc","cd","def","abcd"}; 
+    // TC 2 - No
+    // string s = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef";
+    // vs A {"e","eeee","eeeeeeee","eeeeee"};
+    if (canWord(S,A)) cout << "Yes";
+    else cout << "No";
+}
