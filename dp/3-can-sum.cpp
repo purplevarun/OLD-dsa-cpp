6a350208b3bdc39e66f481c0a32cee042061bcbd
purplevarun <purplevarun@gmail.com>
2021-02-19 21:21:37 +0530

    .

--- dp/3-can-sum.cpp
+++ dp/3-can-sum.cpp
@@ -14,9 +14,9 @@ bool canSum (int target, vi A) {
     return false;
 }
 int32_t main () {
-    // int target = 7; vi A {2,3}; // TC 1 Answer = Yes
+    int target = 7; vi A {2,3}; // TC 1 Answer = Yes
     // int target = 9; vi A {2,4}; // TC 2 Answer = No
-    int target = 300; vi A {7,14}; // TC 3 Answer = No
-    if (canSum(target,A)) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    // int target = 300; vi A {7,14}; // TC 3 Answer = No
+    if (canSum(target,A)) cout << "Yes";
+    else cout << "No";
 }
