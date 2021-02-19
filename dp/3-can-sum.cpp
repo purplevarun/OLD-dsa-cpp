5e8fbf1acc083e9623a848905b7639f06770a268
purplevarun <purplevarun@gmail.com>
2021-02-19 21:06:39 +0530

    .

--- dp/3-can-sum.cpp
+++ dp/3-can-sum.cpp
@@ -5,8 +5,12 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
+bool canSum (int target, vi arr) {
+        
+}
 int32_t main (int32_t argc, char** argv) {
     int target = atoi (argv[1]);
     vi arr;
     for (int i=2;i<argc;i++) arr.pb(atoi(argv[i]));
+    cout << canSum (target,arr) << endl;
 }
