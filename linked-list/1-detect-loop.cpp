68e13ce75717e7cf9739ff88dedf659cc56c0a77
purplevarun <purplevarun@gmail.com>
2021-02-20 09:04:19 +0530

    .

--- linked-list/1-detect-loop.cpp
+++ linked-list/1-detect-loop.cpp
@@ -15,6 +15,24 @@ class node {
     }
 };
 int32_t main () {
-    node* X = new node (5);
-    cout << X->next << endl;
+    node* A = new node (1);
+    node* B = new node (1);
+    node* C = new node (1);
+    node* D = new node (1);
+    A -> next = B;
+    B -> next = C;
+    C -> next = D;
+    D -> next = B;
+    set <node*> seen;
+    bool loop = false;
+    while (A != null) {
+        if (seen.find(A)!=seen.end()){
+            loop = true;
+            break;
+        }
+        seen.insert(A);
+        A = A -> next;
+    }
+    if (loop) cout << "There is loop";
+    else cout << "No loop";
 }
