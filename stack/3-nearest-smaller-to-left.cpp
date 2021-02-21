25faa6ebbb739f3bc813e87e257f9a2bad4362ad
purplevarun <purplevarun@gmail.com>
2021-02-21 15:52:14 +0530

    .

--- stack/3-nearest-smaller-to-left.cpp
+++ stack/3-nearest-smaller-to-left.cpp
@@ -15,31 +15,31 @@ int32_t main () {
     int N = A.size ();
     vi Result;
     //  ----------------------
-    // stack <int> s;
-    // for (int i=0;i<N;i++) {
-    //     if (s.empty() == true) Result.pb(-1);
-    //     else {
-    //         if (s.top() < A[i]) Result.pb(s.top());
-    //         else {
-    //             while (s.empty()==false && s.top() > A[i]) {
-    //                 s.pop ();
-    //             }
-    //             if (s.empty()) Result.pb(-1);
-    //             else Result.pb(s.top());
-    //         }
-    //     }
-    //     s.push(A[i]);
-    // }
-    //  ----------------------
-    // BRUTE FORCE
+    stack <int> s;
     for (int i=0;i<N;i++) {
-        int temp = -1;
-        for (int j=0;j<i;j++) {
-            if (A[j] < A[i]) {
-                temp = A[j];
+        if (s.empty() == true) Result.pb(-1);
+        else {
+            if (s.top() < A[i]) Result.pb(s.top());
+            else {
+                while (s.empty()==false && s.top() > A[i]) {
+                    s.pop ();
+                }
+                if (s.empty()) Result.pb(-1);
+                else Result.pb(s.top());
             }
         }
-        Result.pb(temp);
+        s.push(A[i]);
     }
+    //  ----------------------
+    // BRUTE FORCE
+    // for (int i=0;i<N;i++) {
+    //     int temp = -1;
+    //     for (int j=0;j<i;j++) {
+    //         if (A[j] < A[i]) {
+    //             temp = A[j];
+    //         }
+    //     }
+    //     Result.pb(temp);
+    // }
     for (int i:Result) cout << i << " " ;
 }
