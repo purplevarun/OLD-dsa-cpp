dcb8614b78cf0f99c3957af61ef5fce75f8497ee
purplevarun <purplevarun@gmail.com>
2021-02-21 16:01:52 +0530

    .

--- stack/3-nearest-smaller-to-left.cpp
+++ stack/3-nearest-smaller-to-left.cpp
@@ -10,8 +10,9 @@ using namespace std;
 #define pb push_back
 #define all(x) x.begin(),x.end()
 int32_t main () {
-    vi A {4,5,2,10,8}; // TC 1 Answer = {-1,4,-1,2,2}
+    // vi A {4,5,2,10,8}; // TC 1 Answer = {-1,4,-1,2,2}
     // vi A {1,2,3,4,5}; // TC 2 Answer = {-1,1,2,3,4}
+    vi A {1,1,1,1};
     int N = A.size ();
     vi Result;
     //  ----------------------
@@ -21,7 +22,7 @@ int32_t main () {
         else {
             if (s.top() < A[i]) Result.pb(s.top());
             else {
-                while (s.empty()==false && s.top() > A[i]) {
+                while (s.empty()==false && s.top() >= A[i]) {
                     s.pop ();
                 }
                 if (s.empty()) Result.pb(-1);
