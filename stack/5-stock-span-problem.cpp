6371e038ebee96a8fe6440c953c7d72728c566fd
purplevarun <purplevarun@gmail.com>
2021-02-21 16:18:23 +0530

    .

---
+++ stack/5-stock-span-problem.cpp
@@ -0,0 +1,16 @@
+/*
+    Stock Span
+    Stack Question 5
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define null nullptr
+#define vi vector <int>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+int32_t main () {
+    // IDEA : before any day, number of consecutive smaller or equal to days
+    vi A {100,80,60,70,60,75,85}; vi Answer {1,1,1,2,1,4,6}; // TC 1
+    
+}
