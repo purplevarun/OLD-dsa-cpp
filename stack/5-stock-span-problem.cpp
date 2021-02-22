7242a5f2b148b971488312c85465dd187fcc5d18
purplevarun <purplevarun@gmail.com>
2021-02-22 09:18:28 +0530

    .

--- stack/5-stock-span-problem.cpp
+++ stack/5-stock-span-problem.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #define line cout << '\n';
 int32_t main () {
     // IDEA : before any day, number of consecutive smaller or equal to days
-    // vi A {100,80,60,70,60,75,85}; vi Answer {1,1,1,2,1,4,6}; // TC 1
+    vi A {100,80,60,70,60,75,85}; vi Answer {1,1,1,2,1,4,6}; // TC 1
     // vi A {1,2,3,4,5}; vi Answer {1,2,3,4,5}; // TC 2
     // vi A {5,4,3,2,1}; vi Answer {1,1,1,1,1}; // TC 3
     int N = A.size ();
@@ -52,7 +52,6 @@ int32_t main () {
     //     }
     //     Result.pb(count);
     // }
-    if (Result == Answer) cout << "TC Passed";
-    else cout << "TC Fail";
-    // line for (int i:Result) cout << i << " ";
+    if (Result == Answer) cout << "TC Passed"; else cout << "TC Fail";
+    line for (int i:Result) cout << i << " ";
 }
