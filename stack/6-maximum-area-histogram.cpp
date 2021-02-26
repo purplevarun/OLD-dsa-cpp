4623d60cca63efa10dfac70ba3b1e3c8d1177c0a
purplevarun <purplevarun@gmail.com>
2021-02-26 15:09:41 +0530

    .

--- stack/6-maximum-area-histogram.cpp
+++ stack/6-maximum-area-histogram.cpp
@@ -5,4 +5,22 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
-int32_t main () {}
+int32_t main () {
+    // given : height of bars
+    vi A {6,2,5,4,5,1,6};
+    int N = A.size();
+    // -------------------------
+    int maxArea=0;
+    for (int i=0;i<N;i++) {
+        int count = 1;
+        int height = A[i];
+        int j = i-1;
+        while (j>=0 && A[j]>=height){count++,j--;}
+        j=i+1;
+        while (j<N && A[j]>=height){count++,j++;}
+        int area = count*height;
+        cout << "A[i] = " << A[i] << " and Area = " << area << endl;
+        maxArea = max (maxArea,area);
+    }    
+    cout << maxArea << endl;
+}
