5da518e5a7f04d6e65c3ba8bcb087b9b948a437f
purplevarun <purplevarun@gmail.com>
2021-02-26 15:14:22 +0530

    .

--- stack/6-maximum-area-histogram.cpp
+++ stack/6-maximum-area-histogram.cpp
@@ -7,20 +7,24 @@ using namespace std;
 #define all(x) x.begin(),x.end()
 int32_t main () {
     // given : height of bars
-    vi A {6,2,5,4,5,1,6};
+    // vi A {6,2,5,4,5,1,6}; // TC 1 Answer = 12
+    vi A {1,2,3,4,5}; // TC 2 Answer = 9
     int N = A.size();
     // -------------------------
-    int maxArea=0;
-    for (int i=0;i<N;i++) {
-        int count = 1;
-        int height = A[i];
-        int j = i-1;
-        while (j>=0 && A[j]>=height){count++,j--;}
-        j=i+1;
-        while (j<N && A[j]>=height){count++,j++;}
-        int area = count*height;
-        cout << "A[i] = " << A[i] << " and Area = " << area << endl;
-        maxArea = max (maxArea,area);
-    }    
-    cout << maxArea << endl;
+    
+    // -------------------------
+    // BRUTE FORCE
+    // int maxArea=0;
+    // for (int i=0;i<N;i++) {
+    //     int count = 1;
+    //     int height = A[i];
+    //     int j = i-1; // stack alert
+    //     while (j>=0 && A[j]>=height){count++,j--;}
+    //     j=i+1;
+    //     while (j<N && A[j]>=height){count++,j++;}
+    //     int area = count*height;
+    //     // cout << "A[i] = " << A[i] << " and Area = " << area << endl;
+    //     maxArea = max (maxArea,area);
+    // }    
+    // cout << maxArea << endl;
 }
