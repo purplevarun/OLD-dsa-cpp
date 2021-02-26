caa9ee6ef21fcdfb69850a0a4a8976fedfc0af1a
purplevarun <purplevarun@gmail.com>
2021-02-26 15:00:02 +0530

    .

---
+++ stack/6-maximum-area-histogram.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define null nullptr
+#define vi vector <int>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+int32_t main () {}
