ffe4357103b9875852b0d77afdd32c1c70ddc95d
purplevarun <purplevarun@gmail.com>
2021-02-27 10:18:21 +0530

    .

--- stack/7-rain-water-trapping.cpp
+++ stack/7-rain-water-trapping.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define pb push_back
 #define all(x) x.begin(),x.end()
 int32_t main () {
-    vi A {3,0,0,2,0,4}; // TC 1
-    vi A {0,1,0,2,1,0,1,3,2,1,2,1}; // TC 2
+    vi A {3,0,0,2,0,4}; // TC 1 Answer = 10
+    // vi A {0,1,0,2,1,0,1,3,2,1,2,1}; // TC 2 Answer = 6
+    
 }
