4e308ed47606c8feef3acb822abd5104d9514c39
purplevarun <purplevarun@gmail.com>
2021-03-02 12:33:42 +0530

    .

--- stack/7-maximum-area-rectangle-in-binary-matrix.cpp
+++ stack/7-maximum-area-rectangle-in-binary-matrix.cpp
@@ -10,6 +10,60 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
+int MAH (vi A){
+    int N = A.size();
+    // -------------------------
+    vi Right(N); // to store index of nearest smallest to right
+    vi Left(N); // to store index of nearest smallest to left
+    vi Width(N); // to store eligible widths of each bar
+    vi Area(N); // to store area of each bar
+    stack <pair<int,int>> s;
+    int pseudo_index;
+    // finding NSL indexes
+    pseudo_index = -1;
+    for (int i=0;i<N;i++) {
+        if (s.empty()) {
+            Left[i] = pseudo_index;
+        }
+        else {
+            if (s.top().first < A[i]) {
+                Left[i] = s.top().second;
+            }
+            else {
+                while (s.empty()==false && s.top().first >= A[i]) {
+                    s.pop();
+                }
+                if (s.empty()) Left[i] = pseudo_index;
+                else Left[i] = s.top().second;
+            }
+        }
+        s.push({A[i],i});
+    }
+    while (!s.empty()) s.pop(); // emptying stack
+    // finding NSR indexes
+    pseudo_index = N;
+    for (int i=N-1;i>=0;i--){
+        if (s.empty())
+            Right[i] = pseudo_index;
+        else {
+            if (s.top().first < A[i])
+                Right[i] = s.top().second;
+            else {
+                while (s.empty()==false && s.top().first >= A[i])
+                    s.pop();
+                if (s.empty()) Right[i] = pseudo_index;
+                else Right[i] = s.top().second;
+            }    
+        }
+        s.push({A[i],i});
+    }
+    for (int i=0;i<N;i++){
+        Width[i] = Right[i] - Left[i] - 1;
+        Area[i] = Width[i] * A[i];
+    }
+    return *max_element(all(Area));
+}
 int32_t main () {
     vi A {{0,1,1,0},{1,1,1,1},{1,1,1,1},{1,1,0,0}}; // TC 1 Answer = 8
+    cout << MAH(A[0]);
 }
