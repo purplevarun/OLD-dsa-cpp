85d8f920a869157a5e4899a629a13ad68e83df10
purplevarun <purplevarun@gmail.com>
2021-03-03 09:53:57 +0530

    .

--- stack/9-minimum-element-in-stack.cpp
+++ stack/9-minimum-element-in-stack.cpp
@@ -1,3 +1,6 @@
+/*
+    Minimum element in stack in O(1) space
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long int
@@ -5,6 +8,14 @@ using namespace std;
 #define vi vector <int>
 #define pb push_back
 #define all(x) x.begin(),x.end()
+int minElement;
+stack <int> s;
+int getMin () {
+    if (s.empty()) return -1;
+    else return minElement;
+}
 int32_t main () {
-    
+    vi A {}; // TC 1
+    // for (int i:A)
+    cout << minElement;
 }
