9e3363ca40de0274a8d1ad9f070c3c44175edb3f
purplevarun <purplevarun@gmail.com>
2021-03-08 16:27:04 +0530

    .

--- binary-search/4-count-of-element-in-sorted-array.cpp
+++ binary-search/4-count-of-element-in-sorted-array.cpp
@@ -10,5 +10,9 @@ using namespace std;
 #define all(x) x.begin(),x.end()
 int32_t main () {
     // Solving in O(LogN) time
+    vi A {2,4,10,10,10,18,20};
+    
+    int first = -1, last = -1, high, low, N = A.size();
+    low = 0, high = N-1;
     
 }
