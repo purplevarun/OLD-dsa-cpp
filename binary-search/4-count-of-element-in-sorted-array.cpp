3ce211dfafa2587ea28ea00d23bb9ae8e28f3faa
purplevarun <purplevarun@gmail.com>
2021-03-08 16:34:20 +0530

    .

--- binary-search/4-count-of-element-in-sorted-array.cpp
+++ binary-search/4-count-of-element-in-sorted-array.cpp
@@ -11,11 +11,29 @@ using namespace std;
 int32_t main () {
     // Solving in O(LogN) time
     vi A {2,4,10,10,10,18,20};
-    
+    int X = 10;
     int first = -1, last = -1, high, low, N = A.size();
     low = 0, high = N-1;
     while (low <= high) {
-        int mid = low + (high-low)/2;
-        
+        int mid = low + (high - low)/2;
+        if (X == A[mid]) {
+            first = mid;
+            high = mid-1;
+        }
+        else if (X < A[mid]) high = mid - 1;
+        else if (X > A[mid]) low = mid + 1;
     }
+    low = 0, high = N-1,last=first;
+    while (low <= high) {
+        int mid = low + (high-low)/2;
+        if (X == A[mid]) {
+            last = mid;
+            low = mid+1;
+        }
+        else if (X < A[mid]) high = mid - 1;
+        else if (X > A[mid]) low = mid + 1;
+    }
+    int ans = -1;
+    if (first != -1) ans = last - first + 1;
+    cout << ans;
 }
