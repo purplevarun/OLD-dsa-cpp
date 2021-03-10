0cf9b9d9ed0b373c9baebea30d43d2e8343ced58
purplevarun <purplevarun@gmail.com>
2021-03-11 01:43:10 +0530

    .

--- binary-search/5-number-of-times-sorted-array-is-rotated.cpp
+++ binary-search/5-number-of-times-sorted-array-is-rotated.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 #define int long long int
 #define null nullptr
-#define vi vector <int>
+#define vi vector<int>
 #define pb push_back
-#define all(x) x.begin(),x.end()
-int32_t main () {
-    vi A {8,10,12,15,2,4,6}; // Answer = 3
-    
+#define all(x) x.begin(), x.end()
+int32_t main()
+{
+    vi A{8, 10, 12, 15, 2, 4, 6}; // Answer = 3
+    // find index of smallest element
 }
