a17601d3698abc456a3a5637ea52cc4e6629e576
purplevarun <purplevarun@gmail.com>
2021-03-23 00:05:21 +0530

    .

---
+++ misc/0.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define vi vector<int>
+#define pb push_back
+int32_t main (){
+    clock_t start,end;
+    start = clock();
+    for (int i=0;i<1e10;i++) {
+        int x = 5;
+    }
+    end = clock();
+    cout << fixed <<double(end-start)/double(CLOCKS_PER_SEC) << setprecision(5)<<endl;
+}
