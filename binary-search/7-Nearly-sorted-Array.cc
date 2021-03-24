495a696c1debd0f3c4991ba300b984ad2fbc4424
purplevarun <purplevarun@gmail.com>
2021-03-24 17:09:13 +0530

    .

--- binary-search/7-Nearly-sorted-Array.cc
+++ binary-search/7-Nearly-sorted-Array.cc
@@ -5,6 +5,10 @@ using namespace std;
 #define int long long int
 #define vi vector<int>
 #define pb push_back
+int solve () {
+    // return index of element in nearly sorted array
+}
 int32_t main (){
-    
+    // compare X with mid - 1, mid, mid + 1
+
 }
