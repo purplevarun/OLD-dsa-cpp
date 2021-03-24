d4b0ef1e53954b6967f2c484c2071935c722807d
purplevarun <purplevarun@gmail.com>
2021-03-24 17:00:35 +0530

    .

---
+++ binary-search/7-Nearly-sorted-Array.cc
@@ -0,0 +1,10 @@
+// nearly sorted array
+// find element in nsa
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define vi vector<int>
+#define pb push_back
+int32_t main (){
+    
+}
