c838fe042a86de313a55b8e0f69bdefabc2b8a44
purplevarun <purplevarun@gmail.com>
2021-03-25 02:04:14 +0530

    .

--- binary-search/7-Nearly-sorted-Array.cc
+++ binary-search/7-Nearly-sorted-Array.cc
@@ -4,12 +4,24 @@ using namespace std;
 #define int long long int
 #define vi vector<int>
 #define pb push_back
-int solve (vi A) {
+int solve (vi A, int X) {
     // return index of element in nearly sorted array
+    int N = A.size();
+    int low = 0, high = N-1;
+    int mid;
+    while (low <= high) {
+        mid = low + (high - low)/2;
+        if (A[mid] == X) return mid;
+        if (mid-1 >= low && A[mid-1]==X) return mid-1;
+        if (mid+1 <= high && A[mid+1]==X) return mid+1;
+        if (X > A[mid]) low = mid+1;
+        if (X < A[mid]) high = mid-1;
+    }
+    return -1;
 }
 int32_t main (){
     // compare X with mid - 1, mid, mid + 1
     vi A {10, 3, 40, 20, 50, 80, 70};
-    int X = 40;
-    cout << solve (A);
+    int X = 70;
+    cout << solve (A, X);
 }
