1f2328fe3d3c745a3776c80967b3325d5244c609
purplevarun <purplevarun@gmail.com>
2021-03-25 12:09:35 +0530

    .

---
+++ binary-search/8-floor-of-number-in-array.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define vi vector <int>
+#define pb push_back
+int solve (vi A, int X) {
+	int N = A.size();
+	return 0;
+}
+int32_t main () {
+	// floor of a number is greatest element
+	// smaller than the number
+	vi A {1,2,8,10,10,12,19};
+	int X = 5;
+	int Output = 2;
+	if (solve(A,X) == Output)
+		cout << "Test Case Passed" << endl;
+	else cout << "Test Case Failed" << endl;
+}
