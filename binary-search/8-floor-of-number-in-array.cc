e20645702ab0f2bcd6f806ea7e90ceef1230bd64
purplevarun <purplevarun@gmail.com>
2021-03-26 23:10:12 +0530

    .

--- binary-search/8-floor-of-number-in-array.cc
+++ binary-search/8-floor-of-number-in-array.cc
@@ -5,7 +5,11 @@ using namespace std;
 #define pb push_back
 int solve (vi A, int X) {
 	int N = A.size();
-	
+	int low = 0, high = N-1;
+	while (low <= high) {
+		int mid = low + (high - low)/2;
+
+	}
 	return N;
 }
 int32_t main () {
@@ -13,7 +17,7 @@ int32_t main () {
 	// smaller than the number
 	vi A {1,2,8,10,10,12,19};
 	int X = 5;
-	int Output = 2;
+	int Output = 1; // index of 2 which is the floor of 5
 	if (solve(A,X) == Output)
 		cout << "Test Case Passed" << endl;
 	else cout << "Test Case Failed" << endl;
