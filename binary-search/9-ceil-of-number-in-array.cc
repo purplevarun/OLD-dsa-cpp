ac95dbc5c93f482e955777d5e4e2f20457a8fcd5
purplevarun <purplevarun@gmail.com>
2021-04-01 15:20:43 +0530

    .

--- binary-search/9-ceil-of-number-in-array.cc
+++ binary-search/9-ceil-of-number-in-array.cc
@@ -5,14 +5,26 @@ using namespace std;
 int solve (vi A, int X){
     int N = A.size ();
     int low = 0, high = N-1;
+    int res;
     while (low <= high) {
-        
+        int mid = low + (high-low)/2;
+        if (X == A[mid]) return X;
+        if (X > A[mid]) {
+            low = mid + 1;
+        }
+        if (X < A[mid]) {
+            res = mid;
+            high = mid - 1;
+        }
     }
+    return res;
 }
 int32_t main (){
     vi A {1,2,3,4,8,10,10,12,19};
     int X = 5;
-    int Answer = 8;
-    if (solve (A,X) == Answer) cout << "TC Passed" << endl;
-    else cout << "TC Failed" << endl;
+    int Answer = 4; // index of 8 which is the ceil of 5
+    if (solve (A,X) == Answer) 
+        cout << "TC Passed" << endl;
+    else 
+        cout << "TC Failed" << endl;
 }
