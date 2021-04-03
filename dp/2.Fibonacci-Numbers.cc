748293519a8393b1f7818eae7779181c7ca3ac1c
purplevarun <purplevarun@gmail.com>
2021-04-03 22:06:48 +0530

    .

--- dp/2.Fibonacci-Numbers.cc
+++ dp/2.Fibonacci-Numbers.cc
@@ -2,6 +2,17 @@
 using namespace std;
 #define int int64_t
 #define vi vector <int>
+map <int,int> dp;
+int fibo (int N) {
+    if (dp.find(N)!=dp.end())
+        return dp[N];
+    if (N<=1){
+        return N;
+    }
+    dp[N] = fibo(N-1) + fibo(N-2);
+    return dp[N];
+}
 int32_t main (){
-    
+    int N = 50;
+    cout << fibo (N) << endl;    
 }
