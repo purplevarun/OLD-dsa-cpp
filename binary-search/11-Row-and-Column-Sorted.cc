5c829652e459eff8d51aa60b4d47caac06282a12
purplevarun <purplevarun@gmail.com>
2021-04-04 12:27:47 +0530

    .

---
+++ binary-search/11-Row-and-Column-Sorted.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+#define vi vector <int>
+int32_t main (){
+    vector <vi> {
+        {10,20,30,40},
+        {15,25,35,45},
+        {27,29,37,48},
+        {32,33,39,50}
+    };
+    
+}
