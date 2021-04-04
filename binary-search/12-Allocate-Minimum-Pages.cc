191e7e17482a2eb405825ce902c6bac2d4a5e184
purplevarun <purplevarun@gmail.com>
2021-04-04 21:31:27 +0530

    .

--- binary-search/12-Allocate-Minimum-Pages.cc
+++ binary-search/12-Allocate-Minimum-Pages.cc
@@ -3,7 +3,9 @@ using namespace std;
 #define int int64_t
 #define vi vector <int>
 int solve (vi A, int K) {
-    
+    int low = *max_element(A.begin(),A.end());
+    int high = accumulate(A.begin(),A.end(),0);
+    int result = -1;
 }
 int32_t main (){
     vi A {10,20,30,40};
