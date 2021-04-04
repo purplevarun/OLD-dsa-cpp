d5b37aaa0feaf229c0a1da7a54c948547575baef
purplevarun <purplevarun@gmail.com>
2021-04-04 21:30:27 +0530

    .

--- binary-search/12-Allocate-Minimum-Pages.cc
+++ binary-search/12-Allocate-Minimum-Pages.cc
@@ -2,6 +2,10 @@
 using namespace std;
 #define int int64_t
 #define vi vector <int>
-int32_t main (){
+int solve (vi A, int K) {
     
+}
+int32_t main (){
+    vi A {10,20,30,40};
+    int K = 2;
 }
