142498e00d0eda0f6c850b452038073724ab97ec
purplevarun <purplevarun@gmail.com>
2021-04-04 22:18:41 +0530

    .

--- binary-search/6-find-element-in-rotated-sorted-array.cc
+++ binary-search/6-find-element-in-rotated-sorted-array.cc
@@ -3,28 +3,11 @@ using namespace std;
 #define int int64_t
 #define vi vector<int>
 #define pb push_back
+int minElementIndex (vi A){
+    
+}
 int findElementInRotatedArray(vi A,int X){
-    int low = 0, high = A.size()-1;
-    while (low <= high) {
-        int mid = low + (high - low)/2;
-        if (A[mid] == X) return mid;
-
-        if (A[low] < A[high]) { \
-            // perform normal binary search
-            if (A[mid] > X) high = mid-1;
-            else low = mid+1;
-        }
-        else if (A[mid] < A[low]) {
-            // 
-            if (A[mid] < X && X <= A[high]) low = mid+1;
-            else high = mid-1;
-        }
-        else {
-            if (A[mid] > X && X >= A[low]) high = mid-1;
-            else low = mid+1;
-        }
-    }
-    return -1;
+    int index = minElementIndex (A);
 }
 int32_t main () {
     vi A {11,12,15,18,2,5,6,8};
