dfb4e75afcde81b3e0a5e6187ff8342ac5be2955
purplevarun <purplevarun@gmail.com>
2021-04-04 22:16:51 +0530

    .

--- binary-search/5-number-of-times-sorted-array-is-rotated.cpp
+++ binary-search/5-number-of-times-sorted-array-is-rotated.cpp
@@ -9,29 +9,20 @@ using namespace std;
 #define vi vector<int>
 #define pb push_back
 #define all(x) x.begin(), x.end()
-int32_t main()
-{
-    vi A{10, 12, 15, 2, 4, 6,8}; // Answer = 3
-    // vi A{3,4,5,6,1,2}; // Answer = 4
-
-    // find index of smallest element using BS
+int solve (vi A){
     int N = A.size();
     int low = 0, high = N-1;
-    int ans = 0;
     while (low <= high) {
         if (low == high) {
-            ans = low;
-            break;
+            return low;
         }
         int mid = low + (high - low) / 2;
         
         if (mid < high && A[mid+1] < A[mid]){
-            ans = mid+1;
-            break;
+            return mid+1;
         }
         if (mid > low && A[mid] < A[mid-1]){
-            ans = mid;
-            break;
+            return mid;
         }
         if (A[high] > A[mid]) {
             high = mid-1;
@@ -42,5 +33,13 @@ int32_t main()
             continue;
         }
     }    
-    cout << ans;
+    return -1;
+}
+int32_t main()
+{
+    vi A{10, 12, 15, 2, 4, 6,8}; // Answer = 3
+    // vi A{3,4,5,6,1,2}; // Answer = 4
+
+    // find index of smallest element using BS
+    cout << solve (A) << endl;
 }
