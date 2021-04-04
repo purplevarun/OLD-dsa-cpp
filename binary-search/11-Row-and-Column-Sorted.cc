b690833dafd89ad63f6bb320fac27e19a6129dd8
purplevarun <purplevarun@gmail.com>
2021-04-04 13:09:12 +0530

    .

--- binary-search/11-Row-and-Column-Sorted.cc
+++ binary-search/11-Row-and-Column-Sorted.cc
@@ -3,11 +3,13 @@ using namespace std;
 #define int int64_t
 #define vi vector <int>
 int32_t main (){
-    vector <vi> {
+    vector <vi> A {
         {10,20,30,40},
         {15,25,35,45},
         {27,29,37,48},
         {32,33,39,50}
     };
+    int X = 29;
+    int N = A.size(), M = A[0].size();
     
 }
